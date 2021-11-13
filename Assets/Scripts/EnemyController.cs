2ef720654a848ba83abbc8db96f12b09927163a5
Daniel <itzdanielmichael@gmail.com>
2021-11-13 11:53:16 -0500

    create enemy horizontal movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,32 +6,67 @@ public class EnemyController : MonoBehaviour
 
   public Transform leftPoint, rightPoint;
 
-  private bool movingRight;
+  private bool isMovingRight;
 
   private Rigidbody2D theRB;
+  public SpriteRenderer theSR;
 
   // Start is called before the first frame update
   void Start()
   {
-    initializeRigidBody();
-    initializeRightAndLeftPoints();
+    InitializeRB();
+    InitializeRightAndLeftPoints();
+    InitializeEnemyMovement();
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    HandleEnemyMovement();
   }
 
-  private void initializeRightAndLeftPoints()
+  private void HandleEnemyMovement()
+  {
+    if (isMovingRight)
+    {
+      theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y); // move to right
+
+      theSR.flipX = true; // flip sprite to face right direction (by default the sprite is facing left so true will set it to right)
+
+      if (transform.position.x > rightPoint.position.x)
+      {
+        isMovingRight = false;
+      }
+    }
+    else
+    {
+      // moving to the left
+      theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y); // move to left
+
+      theSR.flipX = false; // flip sprite to face left direction (by default the sprite is facing left so false will set it to face left)
+
+      if (transform.position.x < leftPoint.position.x)
+      {
+        isMovingRight = true;
+      }
+
+    }
+  }
+
+  private void InitializeRB()
+  {
+    theRB = GetComponent<Rigidbody2D>();
+  }
+
+  private void InitializeRightAndLeftPoints()
   {
     // remove the parent so when the frog moves they stay in the same position.
     leftPoint.parent = null;
     rightPoint.parent = null;
   }
 
-  private void initializeRigidBody()
+  private void InitializeEnemyMovement()
   {
-    theRB = GetComponent<Rigidbody2D>();
+    isMovingRight = true;
   }
 }
