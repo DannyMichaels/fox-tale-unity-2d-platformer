6c71ec5810088ac46191766eedb5e5ac06800a57
Daniel <itzdanielmichael@gmail.com>
2021-11-14 10:32:37 -0500

    add comment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
   public float moveSpeed;
   public Rigidbody2D theRB; // the rigid body
   public float jumpForce;
-  public bool isOnGround; // keep track of wether player is on ground or not.
+  public bool isOnGround; // keep track of wether player is on ground or not (only public so I can use it in StompBox.cs).
   public Transform groundCheckPoint;
   public LayerMask whatIsGround;
   private bool canDoubleJump;
