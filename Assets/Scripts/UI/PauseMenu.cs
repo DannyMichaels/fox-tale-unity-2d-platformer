60291ad0450da8180c7dc71aa741b6badffa6f8b
Daniel <itzdanielmichael@gmail.com>
2021-11-14 18:48:23 -0500

    add pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // string levelSelect, mainMenu: the name of the component in the Scene (use inspector tab to add).
+  // for example: in scene Testing 2 the string for mainMenu is Main_Menu
+  public string levelSelect, mainMenu;
 
-    // Update is called once per frame
-    void Update()
+  public GameObject pauseScreen;
+  public bool isPaused;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start"))
     {
-        
+      PauseUnpause();
     }
+  }
+
+
+
+  public void PauseUnpause()
+  {
+    if (isPaused)
+    {
+      // unPause
+      isPaused = false;
+      pauseScreen.SetActive(false);
+      ResumeGame();
+    }
+    else
+    {
+      // pause
+      isPaused = true;
+      pauseScreen.SetActive(true);
+      FreezeGame();
+    }
+  }
+
+  public void LevelSelect()
+  {
+    SceneManager.LoadScene(levelSelect);
+  }
+
+  public void MainMenu()
+  {
+    SceneManager.LoadScene(mainMenu);
+  }
+
+  private void FreezeGame()
+  {
+    Time.timeScale = 0f;
+  }
+
+  private void ResumeGame()
+  {
+    Time.timeScale = 1f;
+  }
 }
