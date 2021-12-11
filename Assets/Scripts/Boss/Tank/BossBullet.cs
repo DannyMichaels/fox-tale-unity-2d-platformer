ce3b2d118253312c9e2fd631dbb626b72c0a1485
Daniel <itzdanielmichael@gmail.com>
2021-12-11 17:44:59 -0500

    update BossBullet

---
+++ Assets/Scripts/Boss/Tank/BossBullet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+  public float speed; // how fast it goes
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    MoveBullet();
+  }
+
+  void MoveBullet()
+  {
+
+    // -speed because it should move to the left on x, multiply with the scale to get a negative or a positive number as to where the bullet should go (left or right)
+    // if localscale is a positive number then x axis would be negative (-speed * posNum) so the bullet would go left, but if localScale is negative then - minus - is a positive number so the bullet would go right.
+    transform.position += new Vector3(-speed * transform.localScale.x * Time.deltaTime, 0f, 0f);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+
+    if (other.CompareTag("Player"))
+    {
+      PlayerHealthController.instance.DealDamage();
+    }
+
+    Destroy(gameObject); // destroy the bullet after hitting
+  }
+}
